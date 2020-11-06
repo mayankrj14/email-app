742632ee528aa68f0b9f6f29574e73a5aa20b808
Vineet Mahajan <vineetmahajan2000@gmail.com>
2020-11-06 07:01:57 +0530

    merged and devided into apps

---
+++ src/home/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+from .models import History, User_Key
+
+admin.site.register(History)
+admin.site.register(User_Key)
--- src/home/extra.py
+++ src/home/extra.py
@@ -2,8 +2,6 @@ from django.contrib.auth.models import User
 from django.shortcuts import (redirect, )
 from django.urls import resolve
 
-import smtplib
-
 from .models import History
 
 def create_user_(request):
@@ -42,34 +40,3 @@ def update_history(request, action='Page Visit'):
     )
     his.save()
 
-def send_mail_(args):
-    #print('mail')
-    from_addr   = args['From']
-    password    = args['Password']
-
-    to_addr     = args['To']
-    cc_addr     = args['CC']
-    bcc_addr    = args['BCC']
-        
-    with smtplib.SMTP('smtp.gmail.com', 587) as server:
-        server.starttls()
-        server.login(from_addr, password)
-
-        SUBJECT = args['Subject']  
-        TEXT = args['Text']  
-        
-        #message = 'Subject: {}\n\n{}'.format(SUBJECT, TEXT)
-        message     = "From: %s\r\n" % from_addr
-        message    += "To: %s\r\n" % ",".join(to_addr)
-        message    += "CC: %s\r\n" % ",".join(cc_addr)
-        message    += "Subject: %s\r\n" % SUBJECT
-        message    += "\r\n" 
-        message    += TEXT
-
-        to_addr    = to_addr + cc_addr + bcc_addr
-
-        #print('message_compiled')
-        #print(message)
-        server.sendmail(from_addr, to_addr, message)
-        #print('sent')
-        
